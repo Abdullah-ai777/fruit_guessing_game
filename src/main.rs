24b7a34c6484a2673b1540d1b93c39c2c993f282
Abdullah-ai777 <ariaz7556@gmail.com>
2024-10-30 20:37:06 +0500

    Abdullah

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::io;
+use rand::Rng; 
+
+fn main() {
+   
+    let arr=["Apple","banana","mango","pineapple","orange"];
+ 
+    loop {
+        let mut randm = rand::thread_rng();
+        let i: usize = randm.gen_range(0..arr.len());
+
+
+       
+        let mut input = String::new();
+        
+        println!("Try to guess the fruit names in this: {:?}",arr);
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        let input = input.trim();
+    
+        if input == arr[i] {
+            println!("You guessed it right!");
+            break;
+        } else {
+            println!("Try again!");
+        }
+    }
+}
